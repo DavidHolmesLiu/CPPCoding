4a087c590db3b16a3b9c03b0ab996f26c948e93a
david <001@nicetech.com>
2025-11-16 02:29:35 +0000

    commit isPrime function.

--- hello.cpp
+++ hello.cpp
@@ -6,5 +6,6 @@ int main(){
     
     //this is the first line in c++!
     cout << "hello world.\n" << endl;
+    cin.get();
     return 0;
 }
---
+++ isPrime.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    for (long long num = 10000000000; num <= 19999999999; num++) {
+
+        bool isPrime = true;
+        long long int i = 2;
+
+        while (i < num) {
+            if (num % i == 0) {
+                isPrime = false;
+                break;
+            }
+            ++i;
+        }
+
+        if (isPrime)
+            cout << num << " PRIME!!!!" << endl;
+        else
+            cout << num << " NOT prime" << endl;
+    }
+    
+    return 0;
+}
